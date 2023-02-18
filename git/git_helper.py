10582cf38dc3f9bbac8bc23f99978d658e66a99e
yougrianes <161180068@smail.nju.edu.cn>
2023-02-18 16:01:38 +0800

    因为网络不是很好所以写了一个重试git push/git pull多次的小脚本，不用手动多次提交

---
+++ git/git_helper.py
@@ -0,0 +1,47 @@
+# 由于网络不是很好，经常pull，push就挂掉，但是反反复复提交又很麻烦，所以感觉做一些常用操作的封装会比较好
+
+import argparse
+import subprocess
+from retry import retry
+from func_timeout import func_timeout, func_set_timeout, FunctionTimedOut
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--pull', action="store_true")
+parser.add_argument('--push', action="store_true")
+
+args = parser.parse_args()
+
+
+@retry(BaseException, delay=5, tries=10)
+def git_pull():
+    # timeout 15secs or raise FunctionTimedOut exception
+    @func_set_timeout(15)
+    def _git_pull():
+        subprocess.run("git pull", shell=True, check=True)
+
+    try:
+        _git_pull()
+    except FunctionTimedOut:
+        print("git pull timeout...")
+        raise FunctionTimedOut
+
+
+@retry(BaseException, delay=5, tries=10)
+def git_push():
+    # timeout 15secs or raise FunctionTimedOut exception
+    @func_set_timeout(15)
+    def _git_push():
+        subprocess.run("git push", shell=True, check=True)
+    try:
+        _git_push()
+    except FunctionTimedOut:
+        print("git push timeout...")
+        raise FunctionTimedOut
+
+
+if __name__ == "__main__":
+    if args.pull:
+        git_pull()
+    if args.push:
+        git_push()
